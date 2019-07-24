f734a9a7b8a8fdb5b5e0eb18cd00c3b1829215a4
ztt <ztchernev@gmail.com>
2019-07-24 15:28:38 +0100

    FSL blank test project setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FSL-Angular-Task'`, () => {
+  it(`should have as title 'Angular-Task'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('FSL-Angular-Task');
+    expect(app.title).toEqual('Angular-Task');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FSL-Angular-Task!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Task!');
   });
 });
